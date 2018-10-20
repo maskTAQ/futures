89f9896e3ab2743be72d81e56145074e7410b866
maskTAQ <tim@@hxonline.tv>
2018-10-20 18:44:06 +0800

    完成退出页面

--- src/pages/Exit.js
+++ src/pages/Exit.js
@@ -1,20 +1,51 @@
-import React from "react";
-import { View, Text, StatusBar } from "react-native";
+import React, { PureComponent } from "react";
+import { View, Image, StatusBar, Platform, BackHandler } from "react-native";
 
 import { welcome as styles } from "./styles";
-//import { iconSource } from "commons";
+import { Button, Visible, Text } from "components";
+import { iconSource } from "commons";
 
-const Exit = () => {
-    return (
-        <View style={styles.container}>
-            <StatusBar
-                backgroundColor={"transparent"}
-                translucent={true}
-                barStyle="light-content"
-            />
-            <Text style={{ fontSize: 30 }}>我是退出页面</Text>
-        </View>
-    );
-};
-
-export default Exit;
+export default class Exit extends PureComponent {
+    state = {
+        timeout: 5
+    };
+    UNSAFE_componentWillMount() {
+        Platform.OS === "android" && this.keepTime();
+    }
+    keepTime = () => {
+        const { timeout } = this.state;
+        if (timeout === 1) {
+            BackHandler.exitApp();
+        } else {
+            this.setState(
+                {
+                    timeout: timeout - 1
+                },
+                () => {
+                    setTimeout(this.keepTime, 1000);
+                }
+            );
+        }
+    };
+    render() {
+        const { timeout } = this.state;
+        return (
+            <View style={styles.container}>
+                <StatusBar
+                    backgroundColor={"transparent"}
+                    translucent={true}
+                    barStyle="light-content"
+                />
+                <Visible show={Platform.OS === "android"}>
+                    <Button style={styles.exit}>
+                        <Text style={styles.exitText}>
+                            0{timeout}
+                            退出
+                        </Text>
+                    </Button>
+                </Visible>
+                <Image source={iconSource.exit} style={styles.exitBg} />
+            </View>
+        );
+    }
+}
